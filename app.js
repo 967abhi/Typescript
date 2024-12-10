21ea96c659caac86160b4b8a3b0751b6ef7d766a
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-12-10 14:58:20 +0530

    type inference with typescript

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var num = 10;
-var str = "abhishek";
-console.log(num);
-console.log(str);
+// // let num =10;
+// // let str:string="abhishek"
+// // console.log(num)
+// // console.log(str)
+// function add(n1:number,n2:number){
+//     return n1+n2;
+// }
+// let result =add(10,20);
+// console.log(result)
+function dummy(a) {
+    return a;
+}
+var result = dummy("hello dummy");
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,4 +1,21 @@
-let num =10;
-let str:string="abhishek"
-console.log(num)
-console.log(str)
+// // // let num =10;
+// // // let str:string="abhishek"
+// // // console.log(num)
+// // // console.log(str)
+
+// // function add(n1:number,n2:number){
+// //     return n1+n2;
+// // }
+
+// // let result =add(10,20);
+// // console.log(result)
+
+// function dummy(a){
+//     return a;
+// }
+// let result = dummy("hello dummy");
+// console.log(result)
+
+let data;
+data=10;
+data="hi"
