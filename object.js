77677e338cc852ae3307f7e598122168f351b696
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-12-10 15:36:04 +0530

    typescript object

---
+++ object.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'abhishek',
+    age: 27,
+};
+var User = {
+    name: "abhinav arora",
+    age: 35,
+    email: "abhinavarora@gmail.com"
+};
+console.warn(person.name);
+console.log(User.name);
---
+++ object.ts
@@ -0,0 +1,17 @@
+
+
+type objtype={name:string; age:number;email:string}
+
+const person:objtype={
+    name:'abhishek',
+    age:27,
+    email:"abh@gmail.com"
+}
+const User:objtype={
+    name:"abhinav arora",
+    age:35,
+    email:"abhinavarora@gmail.com"
+}
+
+console.warn(person.name)
+console.log(User.name)
